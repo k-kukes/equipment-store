16a66f5f34705ef4063f647cfb4e854d319d373e
k-kukes <158488017+k-kukes@users.noreply.github.com>
2025-04-19 23:29:28 -0400

    Added sessionStorage functionality & Styled product cards a bit
    I added the sessionStorage functionality when the product is clicked. Also, adjusted the style of the card and made the display into flex

--- SoccerEquipmentStore/js/modules/productsListing.js
+++ SoccerEquipmentStore/js/modules/productsListing.js
@@ -12,20 +12,25 @@ export function loadProducts(products){
         console.log(product);
 
         const div = document.createElement('div');
-        div.class = "card"
+        div.className = "card"
+        div.style = "width: 17rem; height: 20rem"
         productContainer.appendChild(div);
 
         createNewProduct(div, 'img', product.image, "card-img-top");
         createNewProduct(div, 'div', null, "card-body");
         createNewProduct(cardBodyDiv, 'h5', product.itemTitle, "card-title")
         createNewProduct(cardBodyDiv, 'p', product.itemDescription, "card-text")
+
+        div.addEventListener("click", () => {
+            sessionStorage.setItem(product.itemId,product);
+        })
     });
 }
 
 function createNewProduct(parent, elemName, content, elemClass){
     const newElem = document.createElement(elemName);
     newElem.textContent = content;
-    newElem.class = elemClass;
+    newElem.className = elemClass;
     parent.appendChild(newElem);
 
     if (elemName === 'div') {
@@ -34,11 +39,3 @@ function createNewProduct(parent, elemName, content, elemClass){
 
     return newElem
 }
-
-/* <div class="card" id="product-card">
-<img src="" alt="" class="card-img-top">
-<div class="card-body">
-    <h5 class="card-title">Product</h5>
-    <p class="card-text">Card text</p>
-</div>
-</div> */
