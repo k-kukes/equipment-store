2b39309065b1f2d9cdfdc328b9547b666defb44a
Fahz061 <fahadmalikai061@gmail.com>
2025-05-20 23:30:24 -0400

    css and adding darkmode
    adjustments

--- SoccerEquipmentStore/js/app.js
+++ SoccerEquipmentStore/js/app.js
@@ -6,6 +6,24 @@ import { initProductDetails } from "./modules/productDetails.js";
 import { initCart } from "./modules/cart.js";
 import { initHome } from "./modules/home.js";
 
+window.addEventListener("DOMContentLoaded", () => {
+  const savedTheme = localStorage.getItem("theme");
+  if (savedTheme === "dark") {
+    document.body.classList.add("dark-theme");
+  }
+
+  const toggleButton = document.getElementById("lightModeButton");
+  if (toggleButton) {
+    toggleButton.addEventListener("click", () => {
+      document.body.classList.toggle("dark-theme");
+      localStorage.setItem(
+        "theme",
+        document.body.classList.contains("dark-theme") ? "dark" : "light"
+      );
+    });
+  }
+});
+
 document.addEventListener('DOMContentLoaded', initApp);
 
 async function initApp() {
@@ -30,9 +48,10 @@ async function initApp() {
     }
 
     if (page === "productDetails") {
-        initProductDetails();
+        initProductDetails().then(product => {
+        window.loadedProduct = product;
+     });
     }
-
     if (page === "cart") {
         initCart();
     }
